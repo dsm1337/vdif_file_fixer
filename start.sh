2b165a3f3494ba0304e4d94755a0cca89aacb83a
dsm1337 <yaknikitav@gmail.com>
2023-12-19 11:06:35 +0300

    Add sh files Commit

---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./sep_file /home/yakubov/coding/r7296b_zv_296-0439_ch01_bad.vdif /home/yakubov/coding/separated_files/
---
+++ start2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./addition_file /home/yakubov/coding/r7296b_zv_296-0439_ch01_bad.vdif /home/yakubov/coding/appended_files/r7296b_zv_296-0439_ch01_bad_appended.vdif
